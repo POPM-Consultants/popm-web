7e41d86f36ea4a3e61f988b23052b6478a74996d
Sandesh Ratnawat <77960808+Sandesh3003@users.noreply.github.com>
2023-09-15 17:03:18 +0530

    first commit

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./AboutUs.css"; // Import your custom CSS file for styling
+import { color } from "framer-motion";
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-container" id="About-us">
+      <div className="about-us-cards">
+        <div className="about-us-card">
+          <div className="card-icon">
+            <i className="fas fa-users"></i>
+          </div>
+          <h3 className="card-title">Our Team</h3>
+          <p className="card-description">
+            Meet our dedicated and talented team of professionals.
+          </p>
+        </div>
+        <div className="about-us-card">
+          <div className="card-icon">
+            <i className="fas fa-chart-line"></i>
+          </div>
+          <h3 className="card-title">Our Achievements</h3>
+          <p className="card-description">
+            Discover our milestones and achievements over the years.
+          </p>
+        </div>
+        <div className="about-us-card">
+          <div className="card-icon">
+            <i className="fas fa-history"></i>
+          </div>
+          <h3 className="card-title">Our History</h3>
+          <p className="card-description">
+            Learn about our journey from inception to where we are today.
+          </p>
+        </div>
+      </div>
+      <div className="about-us-content">
+        <h1 className="about-us-title"  style={{ color: 'white' }} >About Us</h1>
+        <p className="about-us-description">
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ src/components/Pricing.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+import { InvitationModal } from "./InvitationModal";
+import { CheckArrowIcon } from "../assets/icons/CheckArrowIcon";
+
+const pricingData = [
+  "Seamless integration",
+  "Real-time data visualization",
+  "Advanced predictive analytics",
+  "Collaborative environment",
+  "Responsive customer support",
+];
+
+export const Pricing = () => {
+  const [isMonthly, setIsMonthly] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleChange = () => {
+    setIsMonthly(!isMonthly);
+  };
+
+  return (
+  <></>
+  );
+};
